92c4261b2b3fc10736d6ae5e1c76c33f6739a498
raresonescu <raresonescu@gmail.com>
2022-07-19 11:37:51 +0300

    add 4 people for the first 4 days split into html js and css files

---
+++ script.js
@@ -0,0 +1,52 @@
+function openForm(day) {
+    document.getElementById("myForm").style.display = "block";
+    alert(`DAY ${day}`);
+    var paragraph = document.getElementById("1");
+    document.getElementById("1").innerHTML = "";
+    var text = document.createTextNode("Aceasta persoana o sa fie cazata in aceasta luna in data de: " + day + ".Luna mea personalizata")
+    paragraph.appendChild(text);
+}
+
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function openForm1(day) {
+    document.getElementById("myForm1").style.display = "block";
+    alert(`DAY ${day}`);
+    var paragraph = document.getElementById("2");
+    document.getElementById("2").innerHTML = "";
+    var text = document.createTextNode("Aceasta persoana o sa fie cazata in aceasta luna in data de: " + day)
+    paragraph.appendChild(text);
+}
+
+function closeForm1() {
+    document.getElementById("myForm1").style.display = "none";
+}
+
+function openForm2(day) {
+    document.getElementById("myForm2").style.display = "block";
+    alert(`DAY ${day}`);
+    var paragraph = document.getElementById("3");
+    document.getElementById("3").innerHTML = "";
+    var text = document.createTextNode("Aceasta persoana o sa fie cazata in aceasta luna in data de: " + day)
+    paragraph.appendChild(text);
+}
+
+function closeForm2() {
+    document.getElementById("myForm2").style.display = "none";
+}
+
+function openForm3(day) {
+    document.getElementById("myForm3").style.display = "block";
+    alert(`DAY ${day}`);
+    var paragraph = document.getElementById("4");
+    document.getElementById("4").innerHTML = "";
+    var text = document.createTextNode("Aceasta persoana o sa fie cazata in aceasta luna in data de: " + day)
+    paragraph.appendChild(text);
+}
+
+function closeForm3() {
+    document.getElementById("myForm3").style.display = "none";
+}
