ce1a6252f1c13d231d72899ae29b4712168c83d2
raresonescu <raresonescu@gmail.com>
2022-07-21 11:53:08 +0300

    se deschid toti pentru data respectiva

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 (function() {
     document.querySelectorAll('.day').forEach(item => {
         item.addEventListener('click', event => {
-            console.log(event.target.dataset.date, event.target.innerText, "Weekend: " + event.currentTarget.classList.contains('weekend'),);
+            openForm(event.target.dataset.date);
+            openForm1(event.target.dataset.date);
+            openForm2(event.target.dataset.date);
+            openForm3(event.target.dataset.date);
         })
-    })
-    // console.log("basa");
-    // document.querySelectorAll('.day').forEach(item => {
-    //     console.log(item)
-    //     item.addEventListener('click', event => {
-    //         openForm(event.target.dataset.date);
-    //         console.log("ss")
-    //         openForm1(event.target.dataset.date);
-    //         alert(event.target.dataset.date);
-    //
-    //     })
-    // });
+    });
 })();
 
 
 function openForm(day) {
     document.getElementById("myForm").style.display = "block";
-    alert(`DAY ${day}`);
+
     var paragraph = document.getElementById("1");
     document.getElementById("1").innerHTML = "";
     var text = document.createTextNode("Aceasta persoana o sa fie cazata in aceasta luna in data de: " + day + ".Luna mea personalizata")
@@ -34,10 +26,9 @@ function closeForm() {
 
 function openForm1(day) {
     document.getElementById("myForm1").style.display = "block";
-    alert(`DAY ${day}`);
     var paragraph = document.getElementById("2");
     document.getElementById("2").innerHTML = "";
-    var text = document.createTextNode("Aceasta persoana o sa fie cazata in aceasta luna in data de: " + day)
+    var text = document.createTextNode("Aceasta persoana o sa fie cazata in aceasta luna in data de: " + day )
     paragraph.appendChild(text);
 }
 
@@ -47,7 +38,6 @@ function closeForm1() {
 
 function openForm2(day) {
     document.getElementById("myForm2").style.display = "block";
-    alert(`DAY ${day}`);
     var paragraph = document.getElementById("3");
     document.getElementById("3").innerHTML = "";
     var text = document.createTextNode("Aceasta persoana o sa fie cazata in aceasta luna in data de: " + day)
@@ -60,7 +50,6 @@ function closeForm2() {
 
 function openForm3(day) {
     document.getElementById("myForm3").style.display = "block";
-    alert(`DAY ${day}`);
     var paragraph = document.getElementById("4");
     document.getElementById("4").innerHTML = "";
     var text = document.createTextNode("Aceasta persoana o sa fie cazata in aceasta luna in data de: " + day)
